9d5ec07ced4661f6dc797daafbd730748ab055b3
Wolf Noble <github@wolfspyre.com>
2013-05-21 18:50:29 -0400

    Update config.php
    remove content

--- authdb/config.php
+++ authdb/config.php
@@ -1,9 +1,1 @@
-<?php
-// db properties
-// grant all on genericdb.* to 'generic_user'@'localhost' identified by 'some-STUPENDOUS1!'
-;
-$dbhost = '10.12.10.30';
-$dbuser = 'generic_user';
-$dbpass = 'some-STUPENDOUS1!';
-$dbname = 'genericdb';
-?>
+#
